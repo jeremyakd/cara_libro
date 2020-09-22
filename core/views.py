73fabc00d4ae1034c8cdf0d69fa0814fc50d0a87
jeremyakd <jeremiasc@exo.com.ar>
2020-09-22 17:23:14 -0300

    Update core to CBV

--- core/views.py
+++ core/views.py
@@ -4,11 +4,8 @@ from django.shortcuts import render
 class HomePageView(TemplateView):
     template_name = "core/home.html"
 
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'title' : "JEREM - LAND"})
 
-#def home(request):
-#    return render(request, "core/home.html")
-
-
-
-def sample(request):
-    return render(request, "core/sample.html")
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
